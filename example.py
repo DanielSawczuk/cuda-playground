168426fb0cc2ba9e34a8918ba3aeb2ee4f377748
Daniel Sawczuk <sdaniel288@gmail.com>
2024-06-26 21:52:52 +0200

    Put kernel code into a separate file, add cmd for ncu profiler (#2)
    Co-authored-by: Daniel Sawczuk <daniel.svchk@gmail.com>

--- example.py
+++ example.py
@@ -3,23 +3,22 @@ import pycuda.driver as drv
 import numpy
 
 from pycuda.compiler import SourceModule
-mod = SourceModule("""
-__global__ void multiply_them(float *dest, float *a, float *b)
-{
-  const int i = threadIdx.x;
-  dest[i] = a[i] * b[i];
-}
-""")
+with open("mul.cu") as src_fp:
+  mod = SourceModule(src_fp.read())
 
 multiply_them = mod.get_function("multiply_them")
 
-a = numpy.random.randn(400).astype(numpy.float32)
-b = numpy.random.randn(400).astype(numpy.float32)
+a_size = 1024 * 38 * 2
+b_size = 1024 * 38 * 2
+
+a = numpy.random.randn(a_size).astype(numpy.float32)
+b = numpy.random.randn(b_size).astype(numpy.float32)
 
 dest = numpy.zeros_like(a)
 multiply_them(
         drv.Out(dest), drv.In(a), drv.In(b),
-        block=(400,1,1), grid=(1,1))
+        block=(1024,1,1), grid=(38 * 2,1))
 
 print(dest-a*b)
+assert numpy.allclose(dest, a*b)
 print(dest)
