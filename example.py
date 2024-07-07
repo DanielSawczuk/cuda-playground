d66d758f093649e13ad200ce41243ac244cf130f
Daniel Sawczuk <sdaniel288@gmail.com>
2024-07-07 23:43:24 +0200

    Add naive wmma based matmul (#3)
    Co-authored-by: Daniel Sawczuk <daniel.svchk@gmail.com>

--- example.py
+++ example.py
@@ -1,24 +1,48 @@
 import pycuda.autoinit
 import pycuda.driver as drv
-import numpy
+import numpy as np
+import math
 
 from pycuda.compiler import SourceModule
-with open("mul.cu") as src_fp:
-  mod = SourceModule(src_fp.read())
 
-multiply_them = mod.get_function("multiply_them")
+with open("matmul.cu") as src_fp:
+    # wmma functions are templated so to use them we need `no_extern_c=True`
+    # (pycuda wraps the whole kernel code (including includes) with `extern C` by defualt)
+    mod = SourceModule(src_fp.read(), no_extern_c=True)  # , options=["-DDEBUG"])
 
-a_size = 1024 * 38 * 2
-b_size = 1024 * 38 * 2
+# because of `no_extern_c=True` we need C++ mangled function name
+# (tip: compile kernel to PTX to check how it should look like)
+multiply_them = mod.get_function("_Z6matmulP6__halfS0_Pfiii")
 
-a = numpy.random.randn(a_size).astype(numpy.float32)
-b = numpy.random.randn(b_size).astype(numpy.float32)
+M = 64
+K = 128
+N = 48
 
-dest = numpy.zeros_like(a)
+a = np.random.randn(M, K).astype(np.float16)
+b = np.random.randn(K, N).astype(np.float16)
+c = np.zeros((M, N), dtype=np.float32)
+
+golden_c = np.matmul(a, b) + c
+
 multiply_them(
-        drv.Out(dest), drv.In(a), drv.In(b),
-        block=(1024,1,1), grid=(38 * 2,1))
+    drv.In(a),
+    drv.In(b),
+    drv.InOut(c),
+    np.int32(M),
+    np.int32(K),
+    np.int32(N),
+    block=(math.ceil(M / 16) * 32, math.ceil(N / 16), 1),
+    grid=(1, 1, 1),
+)
 
-print(dest-a*b)
-assert numpy.allclose(dest, a*b)
-print(dest)
+rtol = 1e-2
+atol = 1e-8
+passed = np.allclose(c, golden_c, rtol=rtol, atol=atol)
+if not passed:
+    err = (atol + rtol * abs(golden_c)) - abs(c - golden_c)
+    max_rerr = np.argmax(err)
+    print(
+        f"Fail, max error: {c.ravel()[max_rerr]} != {golden_c.ravel()[max_rerr]}; using: rtol = {rtol}, atol = {atol}"
+    )
+else:
+    print("Pass")
